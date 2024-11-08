7115bef0fc812f19b8a32f8eda20b65c8b37fae3
Yemi Oduyemi <yhermii@gmail.com>
2024-11-08 09:38:47 +0100

    Project setup

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import express, { Application } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import { db, store } from "./config/index"; 
+import AppError from "./utils/appError";
+// import appRoutes from "./routes/app.route";
+// import userRoutes from "./routes/user.route";
+// import productRoutes from "./routes/product.route";
+import "./config/media.config";
+
+
+dotenv.config();
+const app: Application = express();
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://linkorgnet.com.ng"],
+  credentials: true,
+};
+
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Session configuration
+app.use(session({
+  secret: process.env.SECRET_KEY!,
+  resave: false,
+  saveUninitialized: false,
+  store: store,
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 // 1 hour
+  }
+}));
+
+
+// Routes
+// app.use("/api/v1", appRoutes);
+// app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/products", productRoutes);
+
+
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`The route ${req.originalUrl} with the ${req.method} method does not exist on this server! 💨`, 404));
+});
+
+
+// Global error handling middleware
+app.use((err: AppError, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: "error",
+    message: err.message,
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+  });
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+
+  const PORT: number | string = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+db.on("error", console.error.bind(console, "MongoDB Connection Error:"));
+
+export default app;
